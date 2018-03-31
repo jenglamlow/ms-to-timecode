6afee2de6abf9d08d1368cb6b71c237cf296c74d
Low Jeng Lam <jenglamlow@gmail.com>
2018-03-31 21:38:47 +0800

    add test coverage

--- test/test.js
+++ test/test.js
@@ -18,4 +18,20 @@ describe('#numFormatter', function() {
         var result = msToTimecode(3600000, 10);
         expect(result).to.equal('01:00:00:00');
     });
+
+    it('more than 10 hour conversion', function() {
+        var result = msToTimecode(36000000, 10);
+        expect(result).to.equal('10:00:00:00');
+    });
+
+    it('more than 10 minutes conversion', function() {
+        var result = msToTimecode(600000, 10);
+        expect(result).to.equal('00:10:00:00');
+    });
+
+
+    it('more than 10 frames conversion', function() {
+        var result = msToTimecode(500, 20);
+        expect(result).to.equal('00:00:00:10');
+    });
 });
